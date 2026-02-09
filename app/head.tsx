5ff5930157b7b21597eba0cbb667b2600eff22ac
Jason Ramirez <raiderj77@gmail.com>
2026-02-08 17:53:06 -0800

    Add AdSense meta verification

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,7 @@
 export default function Head() {
   return (
     <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7171402107622932"
-        crossOrigin="anonymous"
-      />
+      <meta name="google-adsense-account" content="ca-pub-7171402107622932" />
     </>
   );
 }
