9d7e700d00076fe76c9e445df8a30e7367900134
Jason Ramirez <raiderj77@gmail.com>
2026-02-13 00:31:16 -0800

    Put Google tag in document head

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JQHRPJ9YLF"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JQHRPJ9YLF');
+            `.trim(),
+          }}
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
